fddb6e2aaf279c9396e29566055eea84d94288db
Craig Anderson <cranderson0110@gmail.com>
2021-10-25 14:12:31 +1300

    The text highlight/state is now controlled by a button!
    And it goes line-by-line, not just top-to-bottom (plus bug fixed for tabs)

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -9,8 +9,8 @@ public class MainController : MonoBehaviour {
 	public TextMeshProUGUI code;
 	public ForestVisualizer forestVisualizer;
 
-	private int pseudocodeState = 0;
-	private int codeState = 0;
+	public int pseudocodeState = -1;
+	public int codeState = -1;
 
 	private string highlightStart = "<mark=#00aeaf33>";
 	//private string highlightStart = "<mark=#0000ff33>"; // pure blue?
@@ -21,14 +21,17 @@ public class MainController : MonoBehaviour {
 		forestVisualizer.AddTree(new List<int> { 4, 5, 6 });
 		forestVisualizer.JoinTrees(new List<int> { 0, 1, 2, 3 }, new List<int> { 4, 5, 6 });
 		forestVisualizer.RemoveTree(new List<int> { 4, 5, 6 });
-		pseudocodeState++;
-		setPseduocodeHighlight();
-		codeState += 3;
-		setCodeHighlight();
 	}
 
 	void setPseduocodeHighlight() {
 		string[] text = pseudocode.text.Split('\n');
+		if (pseudocodeState == text.Length) {
+			pseudocodeState--;
+		}
+		if (pseudocodeState > 0) {
+			text[pseudocodeState - 1] = text[pseudocodeState - 1].Replace(highlightStart, "");
+			text[pseudocodeState - 1] = text[pseudocodeState - 1].Replace(highlightEnd, "");
+		}
 		int tabs = text[pseudocodeState].Split('\t').Length - 1;
 		text[pseudocodeState] = new string('\t', tabs) + highlightStart + text[pseudocodeState].Trim() + highlightEnd;
 		pseudocode.text = string.Join("\n", text);
@@ -36,9 +39,23 @@ public class MainController : MonoBehaviour {
 
 	void setCodeHighlight() {
 		string[] text = code.text.Split('\n');
-		int tabs = text[pseudocodeState].Split('\t').Length - 1;
+		if (codeState == text.Length) {
+			codeState--;
+		}
+		if (codeState > 0) {
+			text[codeState - 1] = text[codeState - 1].Replace(highlightStart, "");
+			text[codeState - 1] = text[codeState - 1].Replace(highlightEnd, "");
+		}
+		int tabs = text[codeState].Split('\t').Length - 1;
 		text[codeState] = new string('\t', tabs) + highlightStart + text[codeState].Trim() + highlightEnd;
 		code.text = string.Join("\n", text);
+	}
+
+	public void Step() {
+		pseudocodeState++;
+		setPseduocodeHighlight();
+		codeState++;
+		setCodeHighlight();
 	}
 
 	void Update() {
