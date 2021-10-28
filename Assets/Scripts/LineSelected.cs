f8499bf93d6f9867f5e7e3c61c4e2f68e41904d3
Craig Anderson <cranderson0110@gmail.com>
2021-10-28 13:11:56 +1300

    Added all the code popup tooltip descriptions
    Line-by-line commentary, yeesh.

--- Assets/Scripts/LineSelected.cs
+++ Assets/Scripts/LineSelected.cs
@@ -11,7 +11,36 @@ public class LineSelected : MonoBehaviour, IPointerExitHandler {
 	public RectTransform canvasRect;
 
 	private List<string> descriptions = new List<string> {
-		"This is the definition of the function, which specifies several key properties. <style=\"Code\">public</style> means that the function is publicly accessible by other code, whilst <style=\"Code\">static</style> ensures that it does not require instantiation to run. <style=\"Code\">int Kruskal</style> says that the function is called 'Kruskal,' and will return a single <style=\"Code\">integer</style> (number). <style=\"Code\">int order</style> is the first parameter it requires, specifying the order of the graph, and <style=\"Code\">List<Edge> edges</style> is naturally a list of the edges between the nodes in the graph."
+		"This is the definition of the function, which specifies several key properties. <style=\"Code\">public</style> means that the function is publicly accessible by other code, whilst <style=\"Code\">static</style> ensures that it does not require instantiation to run. <style=\"Code\">int Kruskal</style> says that the function is called 'Kruskal,' and will return a single <style=\"Code\">integer</style> (number). <style=\"Code\">int order</style> is the first parameter it requires, specifying the order of the graph, and <style=\"Code\">List<Edge> edges</style> is naturally a list of the edges between the nodes in the graph.",
+		"This tricky line is much easier understood in Python code. It's not very important to understand, as it can be much better visualised instead. It constructs a list of lists, each of which holds a single node.",
+		"This queue holds all the edges of the graph, which we need for later. As this is a priority queue, it will create and maintain a sorted order, from least to most by edge distance.",
+		"This loop will grab each <style=\"Code\">edge</style> from the <style=\"Code\">edges</style> given to us as part of the graph, and do something with it.",
+		"Here we do something with the <style=\"Code\">edge</style>, namely adding it to the <style=\"Code\">queue</style>, which ensures it is sorted appropriately.",
+		"This concludes the <style=\"Code\">foreach</style> loop code block",
+		"We'll keep track of the number of edges we've used, so we know when to stop. This is just a number (<style=\"Code\">integer</style>)",
+		"Here's another number (<style=\"Code\">integer</style>), which will track the 'cost' or total distance required to traverse the graph. This is the whole reason for what we're doing!",
+		"Here is where we do the bulk of the work, everything prior was just to set the scene and prepare everything we need. We'll keep doing everything inside this code block over and over, until the <style=\"Code\">used_edges</style> matches the nodes in the graph (minus one because the edges go between the nodes).",
+		"We'll grab the next smallest <style=\"Code\">edge</style> here, so we can more easily do things later on.",
+		"This will define a place to put a <style=\"Code\">tree</style> of nodes, once we find something suitable.",
+		"Ditto from above, it's another tree for when we need to store something later on.",
+		"This <style=\"Code\">foreach</style> loop will go through every single <style=\"Code\">tree</style> in our search <style=\"Code\">forest</style>, executing the checks below.",
+		"The first check we do sees whether the <style=\"Code\">tree</style> we're looking at has the <style=\"Code\">source</style> node, but not the <style=\"Code\">destination</style> node.",
+		"If so, we'll keep it for later.",
+		"The second check is for when the <style=\"Code\">tree</style> we're looking at does not have the <style=\"Code\">source</style> node, but does have the <style=\"Code\">destination</style> node.",
+		"Equally, we'll keep this one for later, too.",
+		"And the last case is for when the <style=\"Code\">tree</style> already has both the <style=\"Code\">source</style> and <style=\"Code\">destination</style> nodes, in which case we don't want to do anything more.",
+		"And we'll just ditch this edge, because it's not needed.",
+		"This concludes the <style=\"Code\">foreach</style> code block that goes through all the <style=\"Code\">tree</style>s in the search <style=\"Code\">forest</style>.",
+		"Now, we only want to go ahead and do this stuff if we've definitely got two <style=\"Code\">tree</style>s to do it with. We can confidently do stuff with them, because of how carefully we picked them earlier. If we didn't find two trees, we'll skip this whole code block.",
+		"Here we merge the two <style=\"Code\">tree</style>'s nodes, by taking the nodes from <style=\"Code\">tree2</style> and adding them to <style=\"Code\">tree1</style>.",
+		"We don't need <style=\"Code\">tree2</style> anymore, because all of it's nodes are a part of <style=\"Code\">tree1</style> now. Thus, we'll <style=\"Code\">remove</style> it.",
+		"We've used an edge! Time to increment (add one) to the number of edges we've used.",
+		"And because we have used this edge, we can remove it from our queue. It's been processed!",
+		"And the last thing to make sure we do, is to update our running total <style=\"Code\">cost</style> (a.k.a. distance).",
+		"This concludes the <style=\"Code\">if</style> statement's code block, and where we'll jump to if we skip it.",
+		"This concludes the <style=\"Code\">while</style> loop's code block, and is where we'll end up once we've used all the edges we need.",
+		"Here is where we <style=\"Code\">return</style> our final result, the <style=\"Code\">cost</style> (a.k.a. distance) of going through all the nodes in the graph.",
+		"It's all done now!"
 	};
 
 	TextMeshProUGUI textMesh;
